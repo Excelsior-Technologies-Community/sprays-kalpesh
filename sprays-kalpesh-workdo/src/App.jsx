100a47847e55dc68272adc9158f2e16e1b66915a
KALPESH PRAJAPATI <kp483368@gmail.com>
2026-03-09 13:56:17 +0530

    Update App.jsx

--- sprays-kalpesh-workdo/src/App.jsx
+++ sprays-kalpesh-workdo/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Navbar from './components/Navbar/Navbar'
 import './App.css'
+import Hero from './components/Hero/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
       <div className="App">
         {/* 1. Call the Navbar at the top */}
         <Navbar />
+        <Hero />
 
         {/* 2. Rest of your page content */}
 
